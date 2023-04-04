8aebdfc5d89ad5e036a25fe56c9c27579ad7a249
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-04 17:41:25 +0900

    refactor: index.css

--- app/src/pages/main/Main.tsx
+++ app/src/pages/main/Main.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
-import {
-  FirstPage,
-  SecondPage,
-  ThirdPage,
-  FourthPage,
-  Container,
-} from './components';
+import React, { lazy, useEffect } from 'react';
+import { FirstPage, Container } from './components';
 import { useSearchParams } from 'react-router-dom';
 import { MainProps } from '../../types/main/fourthPage/type';
+const SecondPage = lazy(() =>
+  import('./components').then(({ SecondPage }) => ({ default: SecondPage })),
+);
+const ThirdPage = lazy(() =>
+  import('./components').then(({ ThirdPage }) => ({ default: ThirdPage })),
+);
+const FourthPage = lazy(() =>
+  import('./components').then(({ FourthPage }) => ({ default: FourthPage })),
+);
 
 function Main({ snapPageNumber, setSnapPageNumber }: MainProps) {
   const contents = [
